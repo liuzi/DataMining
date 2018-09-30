ae9766a5efcb338534a565e0893045471800beed
liuzi <769120167@qq.com>
2018-09-30 23:46:08 +0800

    updated

--- Assignment 1/DBScan/dbscan-template.py
+++ Assignment 1/DBScan/dbscan-template.py
@@ -4,14 +4,12 @@ import os
 import numpy as np
 
 from sklearn.cluster import DBSCAN
+from sklearn.metrics.pairwise import euclidean_distances
 from sklearn import metrics
 from sklearn.datasets.samples_generator import make_blobs
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 import math
-
-UNDEFINED = False
-NOISE = None
 
 
 def read_data(filepath):
@@ -33,45 +31,43 @@ def read_data(filepath):
 
 
 def distance(x, y):
-    return math.sqrt(np.power(x-y,2).sum())
+    dis = abs(x-y)
+    return round(math.sqrt((dis*dis.T).sum()), 3)
 
 
 def is_neighborhood(x, y, eps):
-    return distance(x, y) < eps
+    return distance(x, y) <= eps
 
 
 def range_query(X, point_id, eps):
-    n_points = X.shape[1]
-    seeds = []
-    for i in range(0, n_points):
-        if is_neighborhood(X[:,point_id], X[:,i], eps):
-            seeds.append(i)
+    neighbors = np.array([X[point_id]])
+    for nei_id in range(0,len(X)):
 
-    return seeds
+        if (point_id != nei_id):
+            if is_neighborhood(X[point_id], X[nei_id], eps):
+                neighbors = np.append(neighbors, np.array([X[nei_id]]), axis=0)
+
+    return np.array(neighbors)
 
 
-def expand_cluster(X, assigned_class, point_id, cluster_id, eps, min_points):
-    seeds = range_query(X, point_id, eps)
-    if len(seeds) <= min_points:
-        assigned_class[point_id] = NOISE
-        return False
-    else:
-        assigned_class[point_id] = cluster_id
-        for seed_id in seeds:
-            assigned_class[seed_id] = cluster_id
+def search_neighbors(X, indexes, Neighbors, corepoints, labels, cluster_id, eps, minpts):
+    Seeds = Neighbors[1:]
+    for seed_id in range(0, len(Seeds)):
 
-        while len(seeds) > 0:
-            current_point = seeds[0]
-            neighbors = range_query(X, current_point, eps)
-            if len(neighbors) >= min_points:
-                for i in range(0, len(neighbors)):
-                    neighbor_point = neighbors[i]
-                    if assigned_class[neighbor_point] == UNDEFINED or assigned_class[neighbor_point] == NOISE:
-                        if assigned_class[neighbor_point] == UNDEFINED:
-                            seeds.append(neighbor_point)
-                        assigned_class[neighbor_point] = cluster_id
-            seeds = seeds[1:]
-        return True
+        if tuple(Seeds[seed_id]) in labels:
+            if labels[tuple(Seeds[seed_id])] == -1:
+                labels[tuple(Seeds[seed_id])] = cluster_id
+            else:
+                continue
+        labels[tuple(Seeds[seed_id])] = cluster_id
+        nei_neighbors = range_query(X, indexes[tuple(Seeds[seed_id])], eps)
+        if len(nei_neighbors) >= minpts:
+            # print([indexes[tuple(Seeds[seed_id])]])
+            corepoints.extend([indexes[tuple(Seeds[seed_id])]])
+            # Neighbors = np.append(Neighbors, nei_neighbors, axis=0)
+            labels, corepoints = search_neighbors(X, indexes, nei_neighbors, corepoints, labels, cluster_id, eps, minpts)
+    return labels, corepoints
+
 
 
 # To be implemented
@@ -92,90 +88,146 @@ def dbscan(X, eps, minpts):
 
 	'''
 
+    # cluster_id = 0
+    # labels = {}
+    # corepoints = {}
+    #
+    # for point_id in range(0, len(X)):
+    #     if not tuple(X[point_id]) in labels:
+    #         expand_success, newlabels= expand_cluster(X,labels,point_id,cluster_id,eps,minpts)
+    #         if expand_success:
+    #             corepoints[cluster_id] = point_id
+    #             cluster_id += 1
+    #             labels = newlabels
+    #
+    # label_list = []
+    # for point_id in range(0, len(X)):
+    #     label_list.append(labels[tuple(X[point_id])])
+    #
+    # corepoints_list = []
+    # for i in range(0,len(corepoints)):
+    #     corepoints_list.append(corepoints[i])
+    #
+    # return [label_list, corepoints_list]
+
     cluster_id = 0
-    n_points = X.shape[1]
-    assigned_class = [UNDEFINED] * n_points
-    for point_id in range(0, n_points):
-        x = X[:,point_id]
-        if assigned_class[point_id] == UNDEFINED:
-            if expand_cluster(X, assigned_class, point_id, cluster_id, eps, minpts):
-                cluster_id = cluster_id + 1
-    return assigned_class
-    # for point in X:
-    #     if X not in label:
-    #         continue
-    #     Neighbors = RangeQuery(X, distFunc, point, eps)
-    #     if len(Neighbors) < minpts:
-    #         label[tuple(X)] = -1
-    #         continue
-    #     C += 1
-    #     label[tuple(X)] = C
+    indexes = {}
+    corepoints_list = []
 
+    for point_id in range(0, len(X)):
+        indexes[tuple(X[point_id])] = point_id
 
+    label = {}
 
-	#return [[], []]
+    for point_id in range(0, len(X)):
 
+        if tuple(X[point_id]) in label:
+            continue
+        Neighbors = range_query(X, point_id, eps)
 
+        #TODO
+        if len(Neighbors) < minpts:
+            label[tuple(X[point_id])] = -1
+            continue
+
+        label[tuple(X[point_id])] = cluster_id
+        corepoints_list.append(point_id)
+
+        lablel, corepoints_list = search_neighbors(X, indexes, Neighbors, corepoints_list, label, cluster_id, eps, minpts)
+        # Seeds = Neighbors[1:]
+        #
+        # for seed_id in range(0, len(Seeds)):
+        #
+        #     if tuple(Seeds[seed_id]) in label:
+        #         if label[tuple(Seeds[seed_id])] == -1:
+        #             label[tuple(Seeds[seed_id])] = cluster_id
+        #         else:
+        #             continue
+        #     label[tuple(Seeds[seed_id])] = cluster_id
+        #     nei_neighbors = range_query(X, seed_id, eps)
+        #     if len(nei_neighbors) >= minpts:
+        #         Seeds = np.append(Seeds, nei_neighbors, axis = 0)
+
+        cluster_id += 1
+
+    label_list = []
+    for point_id in range(0, len(X)):
+        label_list.append(label[tuple(X[point_id])])
+
+    # corepoints_list = []
+    # for i in range(0,len(corepoints)):
+    #     corepoints_list.append(corepoints[i])
+
+    # print(label_list)
+    corepoints_list.sort()
+    return [label_list, corepoints_list]
 
 
 def main():
-	
-	# if len(sys.argv) != 4:
-	# 	print("Wrong command format, please follwoing the command format below:")
-	# 	print("python dbscan-template.py data_filepath eps minpts")
-	# 	exit(0)
-    # X = read_data(sys.argv[1])
-	X = np.mat(np.array([[-10.1,-20.3], [2.0, 1.5], [4.3, 4.4], [4.3, 4.6], [4.3, 4.5], [2.0, 1.6], [2.0, 1.4]]))
+    # X = np.array([[-10.1,-20.3], [2.0, 1.5], [4.3, 4.4], [4.3, 4.6], [4.3, 4.5], [2.0, 1.6], [2.0, 1.4]])
+    # print(dbscan(X,  0.1, 3))
 
-	# Compute DBSCAN
-    #db = dbscan(X, float(sys.argv[2]), int(sys.argv[3]))
-	print(dbscan(X, 0.1, 3))
+    # X = read_data("/Users/lynnjiang/liuGit/DataMining/Assignment 1/DBScan/Data/data_1.txt")
+    # label_list, corepoints_list = dbscan(X, 0.3, 10)
+    # print(label_list)
+    # print(corepoints_list)
 
-	# # store output labels returned by your algorithm for automatic marking
-	# with open('.'+os.sep+'Output'+os.sep+'labels.txt', "w") as f:
-	# 	for e in db[0]:
-	# 		f.write(str(e))
-	# 		f.write('\n')
-    #
-	# # store output core sample indexes returned by your algorithm for automatic marking
-	# with open('.'+os.sep+'Output'+os.sep+'core_sample_indexes.txt', "w") as f:
-	# 	for e in db[1]:
-	# 		f.write(str(e))
-	# 		f.write('\n')
-    #
-	# _,dimension = X.shape
-    #
-	# # plot the graph is the data is dimensiont 2
-	# if dimension == 2:
-	# 	core_samples_mask = np.zeros_like(np.array(db[0]), dtype=bool)
-	# 	core_samples_mask[db[1]] = True
-	# 	labels = np.array(db[0])
-    #
-	# 	# Number of clusters in labels, ignoring noise if present.
-	# 	n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-    #
-	# 	# Black removed and is used for noise instead.
-	# 	unique_labels = set(labels)
-	# 	colors = [plt.cm.Spectral(each)
-	# 	          for each in np.linspace(0, 1, len(unique_labels))]
-    #
-    #
-	# 	for k, col in zip(unique_labels, colors):
-	# 	    if k == -1:
-	# 	        # Black used for noise.
-	# 	        col = [0, 0, 0, 1]
-    #
-	# 	    class_member_mask = (labels == k)
-    #
-	# 	    xy = X[class_member_mask & core_samples_mask]
-	# 	    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
-	# 	             markeredgecolor='k', markersize=14)
-    #
-	# 	    xy = X[class_member_mask & ~core_samples_mask]
-	# 	    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
-	# 	             markeredgecolor='k', markersize=6)
-    #
-	# 	plt.title('Estimated number of clusters: %d' % n_clusters_)
-	# 	plt.savefig('.'+os.sep+'Output'+os.sep+'cluster-result.png')
+    if len(sys.argv) != 4:
+        print("Wrong command format, please follwoing the command format below:")
+        print("python dbscan-template.py data_filepath eps minpts")
+        exit(0)
+
+    X = read_data(sys.argv[1])
+
+    # Compute DBSCAN
+    db = dbscan(X, float(sys.argv[2]), int(sys.argv[3]))
+
+    # store output labels returned by your algorithm for automatic marking
+    with open('.' + os.sep + 'Output' + os.sep + 'labels.txt', "w") as f:
+        for e in db[0]:
+            f.write(str(e))
+            f.write('\n')
+
+    # store output core sample indexes returned by your algorithm for automatic marking
+    with open('.' + os.sep + 'Output' + os.sep + 'core_sample_indexes.txt', "w") as f:
+        for e in db[1]:
+            f.write(str(e))
+            f.write('\n')
+
+    _, dimension = X.shape
+
+    # plot the graph is the data is dimensiont 2
+    if dimension == 2:
+        core_samples_mask = np.zeros_like(np.array(db[0]), dtype=bool)
+        core_samples_mask[db[1]] = True
+        labels = np.array(db[0])
+
+        # Number of clusters in labels, ignoring noise if present.
+        n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+
+        # Black removed and is used for noise instead.
+        unique_labels = set(labels)
+        colors = [plt.cm.Spectral(each)
+                  for each in np.linspace(0, 1, len(unique_labels))]
+
+        for k, col in zip(unique_labels, colors):
+            if k == -1:
+                # Black used for noise.
+                col = [0, 0, 0, 1]
+
+            class_member_mask = (labels == k)
+
+            xy = X[class_member_mask & core_samples_mask]
+            plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+                     markeredgecolor='k', markersize=14)
+
+            xy = X[class_member_mask & ~core_samples_mask]
+            plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+                     markeredgecolor='k', markersize=6)
+
+        plt.title('Estimated number of clusters: %d' % n_clusters_)
+        plt.savefig('.' + os.sep + 'Output' + os.sep + 'cluster-result.png')
+
+
 
 main()
